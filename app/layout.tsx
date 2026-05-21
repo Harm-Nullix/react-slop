ba4596e18ca654cd21dd1cbd80b922943ae8f810
harmzeinstra <harm@nullix.nl>
2026-05-21 09:31:35 +0200

    update images

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Sloppy App',
-  description: 'Created with 0 love',
+  title: 'Sloppy Short App',
+  description: 'Created with zer0 love',
   icons: {
     icon: [
       {
